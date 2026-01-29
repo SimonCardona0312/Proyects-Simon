6e3ea31ca3a7ca3a2b86111ead2684d90cd1b80c
SimonCardona0312 <traineeroman03@gmail.com>
2026-01-29 15:34:20 -0500

    Update App.py

--- App.py
+++ App.py
@@ -42,7 +42,7 @@ def crear_pptx(texto):
     return pptx_io.getvalue()
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Transcription Function
-Audio_fill = st.file_uploader("Upload your audio so we can transcribe", type=["mp3", "mp4" ,"wav", "opus" , "m4a"])
+Audio_fill = st.file_uploader("Upload your audio so we can transcribe", type=["mp3", "mp4" ,"wav", "opus" , "aac" ,"m4a"])
 
 if Audio_fill is not None:
 
@@ -133,6 +133,7 @@ if Audio_fill is not None:
                 use_container_width=True # Esto hace que el botón ocupe todo el ancho
             )
             st.balloons()
+
 
 
 
